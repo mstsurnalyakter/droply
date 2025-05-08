4915bcaf26c402a129f8a4c45e9a5f47b1187df7
mstsurnalyakter <surnalyms@gmail.com>
2025-05-08 16:22:55 +0600

    feat: add file management schema with relationships

---
+++ src/app/lib/db/schema.ts
@@ -0,0 +1,46 @@
+
+
+import {pgTable,text,uuid, integer,boolean,timestamp} from "drizzle-orm/pg-core";
+import {relations} from "drizzle-orm";
+
+
+export const files = pgTable("files",{
+    id:uuid("id").defaultRandom().primaryKey(),
+    //basic file/folder information
+    name:text("name").notNull(),
+    path:text("path").notNull(),// /document/project/resume
+    size:integer("size").notNull(),
+    type:text("type").notNull(),//"folder"
+
+    //storage information
+    fileUrl:text("file_url").notNull(),// url to access file
+    thumbnailUrl:text("thumbnail_url"),
+
+    //Ownship information
+    userId:text("user_id").notNull(),
+    parentId:uuid("parent_id"),//  parent folder if (null for root items)
+     
+    // file/folder flags
+    isFolder:boolean("is_folder").default(false).notNull(),
+    isStarred:boolean("is_starred").default(false).notNull(),
+    isTrash:boolean("is_trash").default(false).notNull(),
+
+    // timestamps
+    createdAt:timestamp("created_at").defaultNow().notNull(),
+    updatedAt:timestamp("updated_at").defaultNow().notNull()
+})
+
+/**
+ * parent: Each file/folder can have one parent folder
+ * children: Each folder can have many child files/folder
+ */
+
+export const filesRelations = relations(files,({one, many})=>({
+    parent:one(files,{
+        fields:[files.parentId],
+        references:[files.id]
+    }),
+
+    //relationshipt to child file/folder
+    children:many(files)
+}))
