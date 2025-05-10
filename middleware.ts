c17f5009b7eca46769daab007e33f35ed942bb41
mstsurnalyakter <surnalyms@gmail.com>
2025-05-10 07:24:23 +0600

    feat: integrate ImageKit for image management and add authentication route

--- middleware.ts
+++ middleware.ts
@@ -1,34 +1,33 @@
-import { clerkMiddleware,createRouteMatcher,auth } from '@clerk/nextjs/server'
-import { NextResponse } from 'next/server';
-
-
-
-const isPublicRoute = createRouteMatcher([
-    "/",
-    "/sign-up(.*)",
-    "/sign-in(.*)"
-])
-
-export default clerkMiddleware(async (auth,request)=>{
-    const user = auth()
+import {
+    clerkMiddleware,
+    createRouteMatcher,
+    auth,
+  } from "@clerk/nextjs/server";
+  import { NextResponse } from "next/server";
+  
+  const isPublicRoute = createRouteMatcher(["/", "/sign-in(.*)", "/sign-up(.*)"]);
+  
+  export default clerkMiddleware(async (auth, request) => {
+    const user = auth();
     const userId = (await user).userId;
     const url = new URL(request.url);
-    
+  
     if (userId && isPublicRoute(request) && url.pathname !== "/") {
-        return NextResponse.redirect(new URL("/dashboard",request.url))
+      return NextResponse.redirect(new URL("/dashboard", request.url));
     }
-
-    //project non-public route
+  
+    // Protect non-public routes
     if (!isPublicRoute(request)) {
-        await auth.protect()
+      await auth.protect();
     }
-})
-
-export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
-}
+  });
+  
+  export const config = {
+    matcher: [
+      // Skip Next.js internals and all static files, unless found in search params
+      "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+      // Always run for API routes
+      "/(api|trpc)(.*)",
+    ],
+  };
+  
