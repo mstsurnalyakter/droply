2e3c2d6a40dad8fcc7e69e2dd43b5b6668a5d9e7
mstsurnalyakter <surnalyms@gmail.com>
2025-05-10 07:34:34 +0600

    feat: implement ImageKit authentication parameters retrieval in API route

--- src/app/api/imagekit-auth/route.ts
+++ src/app/api/imagekit-auth/route.ts
@@ -3,7 +3,23 @@ import { NextResponse } from "next/server";
 import ImageKit from "imagekit"
 
 const imagekit = new ImageKit({
-    publicKey:process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
-    privateKey:process.env.IMAGEKIT_PRIVATE_KEY,
-    urlEndpoint:process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT
+    publicKey:process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY || "",
+    privateKey:process.env.IMAGEKIT_PRIVATE_KEY || "",
+    urlEndpoint:process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT || ""
 })
+
+export const GET = async () =>{
+    try {
+        const  {userId} = await auth();
+        if (!userId) {
+            return NextResponse.json({error:"Unauthorized"},{status:401})
+        }
+    
+        const authParams = imagekit.getAuthenticationParameters()
+        return NextResponse.json(authParams)
+    
+        
+    } catch (error:any) {
+        return NextResponse.json({error:"Failed to generate authentication parameters for imagekit"},{status:500})
+    }
+}
